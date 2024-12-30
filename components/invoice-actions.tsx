d062e5cb9f50181b3b9c352bd757f0b74a53ae46
Gaks3 <dwi66116@gmail.com>
2024-12-30 19:13:58 +0700

    feat: create invoice actions component

---
+++ components/invoice-actions.tsx
@@ -0,0 +1,60 @@
+import {
+  CheckCircleIcon,
+  DownloadCloudIcon,
+  MailIcon,
+  MoreHorizontalIcon,
+  PencilIcon,
+  TrashIcon,
+} from 'lucide-react'
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import Link from 'next/link'
+
+export function InvoiceActions() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size={'icon'} variant={'secondary'}>
+          <MoreHorizontalIcon className='size-4' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem asChild>
+          <Link href={''}>
+            <CheckCircleIcon className='size-4 mr-2' />
+            Mark as Paid
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href={''}>
+            <PencilIcon className='size-4 mr-2' />
+            Edit Invoice
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href={''}>
+            <DownloadCloudIcon className='size-4 mr-2' />
+            Download Invoice
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href={''}>
+            <MailIcon className='size-4 mr-2' />
+            Reminder Email
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href={''} className='text-destructive'>
+            <TrashIcon className='size-4 mr-2' />
+            Delete Invoice
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
