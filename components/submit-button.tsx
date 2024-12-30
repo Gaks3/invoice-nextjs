05751f2961ba03cf8451a0a46a6804249fb2acea
Gaks3 <dwi66116@gmail.com>
2024-12-30 20:49:55 +0700

    feat: add variant props to choose style button

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -6,13 +6,30 @@ import { Loader2Icon } from 'lucide-react'
 
 interface SubmitButtonProps {
   text?: string
+  variant?:
+    | 'default'
+    | 'destructive'
+    | 'outline'
+    | 'secondary'
+    | 'ghost'
+    | 'link'
+    | null
+    | undefined
 }
 
-export default function SubmitButton({ text = 'Submit' }: SubmitButtonProps) {
+export default function SubmitButton({
+  text = 'Submit',
+  variant,
+}: SubmitButtonProps) {
   const { pending } = useFormStatus()
 
   return (
-    <Button type='submit' className='w-full' disabled={pending}>
+    <Button
+      type='submit'
+      className='w-full'
+      disabled={pending}
+      variant={variant}
+    >
       {pending ? (
         <>
           <Loader2Icon className='size-4 mr-2 animate-spin' /> Please wait...
