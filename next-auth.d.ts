f6ff89bb93b36aebd498eeb907e3b1d224e5d540
Gaks3 <dwi66116@gmail.com>
2024-12-30 19:05:54 +0700

    feat: create custom type user session auth

---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+import { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    email: string
+    firstName?: string
+    lastName?: string
+    image?: string
+  }
+
+  interface Session {
+    user: User & DefaultSession['user']
+    expires: string
+    error: string
+  }
+}
